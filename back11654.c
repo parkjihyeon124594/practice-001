6876e27e5a868e7dad7233ed2a8a58deaa2ef5b9
parkjihyeon124594 <91263647+parkjihyeon124594@users.noreply.github.com>
2021-09-23 22:03:02 +0900

    Add files via upload

---
+++ back11654.c
@@ -0,0 +1,16 @@
+//2021-09-23
+// 백준 11654번 
+//문제:알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+
+	char ch;
+	scanf("%c",&ch);
+	printf("%d",ch);
+
+	return 0;
+}
